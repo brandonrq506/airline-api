e61a0fa2ab33e268c48dc2c8870d9aefc61db580
Brandon Ramirez <darkrahd@gmail.com>
2023-05-08 01:09:10 -0600

    Added proper error handling and AsyncWrapper for controllers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+import errorHandler from "./middleware/errorHandler.js";
 import { corsOptions } from "./config/cors.js";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-// import v1-routes from "./routes/v1-routes.js";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -19,15 +19,18 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-//If I import routes here, I will have to add the /api/v1 to all of them + the route
-//If I have that in a different folder then I can just use the routes as they are.
-//The problem is, I will have to use my middlewares in the routes file, which is not ideal.
 app.get('/api/v1', (req, res) => {
     res.send('Hello to Memories API');
 });
 
 
+app.use(errorHandler);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err));
+
+/*
+Add custome error message to Schema validation.
+
+*/
