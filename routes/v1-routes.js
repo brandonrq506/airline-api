a027872a9f97cab22e2eac19d06f15a6cfdaa66a
Brandon Ramirez <darkrahd@gmail.com>
2023-05-09 17:25:01 -0600

    Added Airport Service, Controller and Routes, added to V1 routes and tested it

--- routes/v1-routes.js
+++ routes/v1-routes.js
@@ -4,12 +4,14 @@ import verifyJwt from '../middleware/verifyJwt.js';
 import customerRoutes from './customer.js'
 import sessionRoutes from './session.js'
 import countryRoutes from './country.js'
+import airportRoutes from './airport.js'
 import cardRoutes from './card.js'
 
 const router = express.Router();
 
 router.use('/auth', sessionRoutes);
 router.use('/countries', countryRoutes);
+router.use('/airports', airportRoutes);
 
 router.use(verifyJwt);
 router.use('/customers', customerRoutes);
