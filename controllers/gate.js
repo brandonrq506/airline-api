82cc1ee3ffffd9cc4ea05e1b0db45a822a279f43
Brandon Ramirez <darkrahd@gmail.com>
2023-05-12 13:13:53 -0600

    Fixed inconsistencies on Card Service and Controller

---
+++ controllers/gate.js
@@ -0,0 +1,49 @@
+import gateService from '../services/gate.js';
+import createError from '../helpers/errorHelpers.js'
+import asyncHandler from '../middlewares/asyncHandler.js';
+
+export const getGates = asyncHandler(async (req, res, next) => {
+    const gates = await gateService.getGates();
+    res.status(200).json(gates);
+});
+
+export const getGate = asyncHandler(async (req, res, next) => {
+    const gateId = req.params.id;
+    const gate = await gateService.getGate(gateId);
+
+    if (!gate)
+        return next(createError(404, `Gate not found`));
+
+    res.status(200).json(gate);
+});
+
+export const createGate = asyncHandler(async (req, res, next) => {
+    const gate = req.body;
+    const newGate = await gateService.createGate(gate);
+
+    if (!newGate)
+        return next(createError(400, `Gate could not be created`));
+
+    res.status(201).json(newGate);
+});
+
+export const updateGate = asyncHandler(async (req, res, next) => {
+    const gateId = req.params.id;
+    const gate = req.body;
+    const updatedGate = await gateService.updateGate(gateId, gate);
+
+    if (!updatedGate)
+        return next(createError(404, `Gate not found`));
+
+    res.status(200).json(updatedGate);
+});
+
+export const deleteGate = asyncHandler(async (req, res, next) => {
+    const gateId = req.params.id;
+    const deletedGate = await gateService.deleteGate(gateId);
+
+    if (!deletedGate)
+        return next(createError(404, `Gate not found`));
+
+    res.status(200).json(deletedGate);
+});
---
+++ services/gate.js
@@ -0,0 +1,58 @@
+import Gate from '../models/Gate.js';
+
+const getGates = async () => {
+    try {
+        const gates = await Gate.find().lean().exec();
+        return gates;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const getGate = async (id) => {
+    try {
+        const gate = await Gate.findById(id).lean().exec();
+        return gate;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const createGate = async (gate) => {
+    try {
+        const newGate = await Gate.create(gate);
+        return newGate;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateGate = async (id, gate) => {
+    try {
+        const newGate = await Gate.findByIdAndUpdate(id, gate,
+            {
+                new: true,
+                runValidators: true
+            }
+        ).exec();
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteGate = async (id) => {
+    try {
+        const gate = await Gate.findByIdAndDelete(id).lean().exec();
+        return gate;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default {
+    getGates,
+    getGate,
+    createGate,
+    updateGate,
+    deleteGate
+};
